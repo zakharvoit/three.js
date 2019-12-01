86f12f4918c848046f9a9b84067ccdc3ce1dd4ce
Diego Marcos <diego.marcos@gmail.com>
2019-12-01 00:11:06 -0500

    Reintroduce WebVRManager for browsers without WebXR support

--- src/renderers/WebGLRenderer.js
+++ src/renderers/WebGLRenderer.js
@@ -55,6 +55,7 @@ import { WebGLState } from './webgl/WebGLState.js';
 import { WebGLTextures } from './webgl/WebGLTextures.js';
 import { WebGLUniforms } from './webgl/WebGLUniforms.js';
 import { WebGLUtils } from './webgl/WebGLUtils.js';
+import { WebVRManager } from './webvr/WebVRManager.js';
 import { WebXRManager } from './webxr/WebXRManager.js';
 import { WebGLMaterials } from './webgl/WebGLMaterials.js';
 import { WebGLUniformsGroups } from './webgl/WebGLUniformsGroups.js';
@@ -372,7 +373,7 @@ class WebGLRenderer {
 
 		// xr
 
-		const xr = new WebXRManager( _this, _gl );
+		const xr = ( typeof navigator !== 'undefined' && 'xr' in navigator ) ? new WebXRManager( _this, _gl ) : new WebVRManager( _this );
 
 		this.xr = xr;
 
@@ -1150,6 +1151,11 @@ class WebGLRenderer {
 
 			if ( scene.isScene === true ) scene.onAfterRender( _this, scene, camera );
 
+			if ( xr.enabled && xr.submitFrame ) {
+
+				xr.submitFrame();
+
+			}
 			// _gl.finish();
 
 			bindingStates.resetDefaultState();
