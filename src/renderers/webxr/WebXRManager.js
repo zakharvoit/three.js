9def41545ae2991c322769022257082961a4ed69
Diego Marcos <diego.marcos@gmail.com>
2020-01-23 10:33:29 -0800

    Add method to set camera pose target on WebXRManager

--- src/renderers/webxr/WebXRManager.js
+++ src/renderers/webxr/WebXRManager.js
@@ -22,6 +22,7 @@ function WebXRManager( renderer, gl ) {
 	var referenceSpaceType = 'local-floor';
 
 	var pose = null;
+	var poseTarget = null;
 
 	var controllers = [];
 	var inputSourcesMap = new Map();
@@ -299,10 +300,17 @@ function WebXRManager( renderer, gl ) {
 
 	}
 
+	this.setPoseTarget = function ( object ) {
+
+		if ( object !== undefined ) poseTarget = object;
+
+	};
+
 	this.getCamera = function ( camera ) {
 
 		var parent = camera.parent;
 		var cameras = cameraVR.cameras;
+		var object = poseTarget || camera;
 
 		updateCamera( cameraVR, parent );
 
@@ -314,9 +322,9 @@ function WebXRManager( renderer, gl ) {
 
 		// update camera and its children
 
-		camera.matrixWorld.copy( cameraVR.matrixWorld );
+		object.matrixWorld.copy( cameraVR.matrixWorld );
 
-		var children = camera.children;
+		var children = object.children;
 
 		for ( var i = 0, l = children.length; i < l; i ++ ) {
 
