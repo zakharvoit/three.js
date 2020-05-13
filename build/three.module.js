5836941a6d8db262d67cd98698fec180739447f9
Diego Marcos <diego.marcos@gmail.com>
2020-05-13 13:15:02 -0700

    Bump build

--- build/three.module.js
+++ build/three.module.js
@@ -22867,6 +22867,26 @@ function WebGLUtils( gl, extensions, capabilities ) {
  * @author mrdoob / http://mrdoob.com/
  */
 
+function Group() {
+
+	Object3D.call( this );
+
+	this.type = 'Group';
+
+}
+
+Group.prototype = Object.assign( Object.create( Object3D.prototype ), {
+
+	constructor: Group,
+
+	isGroup: true
+
+} );
+
+/**
+ * @author mrdoob / http://mrdoob.com/
+ */
+
 function ArrayCamera( array ) {
 
 	PerspectiveCamera.call( this );
@@ -22884,24 +22904,502 @@ ArrayCamera.prototype = Object.assign( Object.create( PerspectiveCamera.prototyp
 } );
 
 /**
+ * @author jsantell / https://www.jsantell.com/
  * @author mrdoob / http://mrdoob.com/
  */
 
-function Group() {
+var cameraLPos = new Vector3();
+var cameraRPos = new Vector3();
 
-	Object3D.call( this );
+/**
+ * Assumes 2 cameras that are parallel and share an X-axis, and that
+ * the cameras' projection and world matrices have already been set.
+ * And that near and far planes are identical for both cameras.
+ * Visualization of this technique: https://computergraphics.stackexchange.com/a/4765
+ */
+function setProjectionFromUnion( camera, cameraL, cameraR ) {
 
-	this.type = 'Group';
+  cameraLPos.setFromMatrixPosition( cameraL.matrixWorld );
+  cameraRPos.setFromMatrixPosition( cameraR.matrixWorld );
+
+  var ipd = cameraLPos.distanceTo( cameraRPos );
+
+  var projL = cameraL.projectionMatrix.elements;
+  var projR = cameraR.projectionMatrix.elements;
+
+  // VR systems will have identical far and near planes, and
+  // most likely identical top and bottom frustum extents.
+  // Use the left camera for these values.
+  var near = projL[ 14 ] / ( projL[ 10 ] - 1 );
+  var far = projL[ 14 ] / ( projL[ 10 ] + 1 );
+  var topFov = ( projL[ 9 ] + 1 ) / projL[ 5 ];
+  var bottomFov = ( projL[ 9 ] - 1 ) / projL[ 5 ];
+
+  var leftFov = ( projL[ 8 ] - 1 ) / projL[ 0 ];
+  var rightFov = ( projR[ 8 ] + 1 ) / projR[ 0 ];
+  var left = near * leftFov;
+  var right = near * rightFov;
+
+  // Calculate the new camera's position offset from the
+  // left camera. xOffset should be roughly half `ipd`.
+  var zOffset = ipd / ( - leftFov + rightFov );
+  var xOffset = zOffset * - leftFov;
+
+  // TODO: Better way to apply this offset?
+  cameraL.matrixWorld.decompose( camera.position, camera.quaternion, camera.scale );
+  camera.translateX( xOffset );
+  camera.translateZ( zOffset );
+  camera.matrixWorld.compose( camera.position, camera.quaternion, camera.scale );
+  camera.matrixWorldInverse.getInverse( camera.matrixWorld );
+
+  // Find the union of the frustum values of the cameras and scale
+  // the values so that the near plane's position does not change in world space,
+  // although must now be relative to the new union camera.
+  var near2 = near + zOffset;
+  var far2 = far + zOffset;
+  var left2 = left - xOffset;
+  var right2 = right + ( ipd - xOffset );
+  var top2 = topFov * far / far2 * near2;
+  var bottom2 = bottomFov * far / far2 * near2;
+
+  camera.projectionMatrix.makePerspective( left2, right2, top2, bottom2, near2, far2 );
 
 }
 
-Group.prototype = Object.assign( Object.create( Object3D.prototype ), {
+/**
+ * @author mrdoob / http://mrdoob.com/
+ */
 
-	constructor: Group,
+function WebVRManager( renderer ) {
 
-	isGroup: true
+	var renderWidth, renderHeight;
+	var scope = this;
 
-} );
+	var device = null;
+	var frameData = null;
+
+	var poseTarget = null;
+
+	var controllers = [];
+	var standingMatrix = new Matrix4();
+	var standingMatrixInverse = new Matrix4();
+
+	var framebufferScaleFactor = 1.0;
+
+	var referenceSpaceType = 'local-floor';
+
+	if ( typeof window !== 'undefined' && 'VRFrameData' in window ) {
+
+		frameData = new window.VRFrameData();
+		window.addEventListener( 'vrdisplaypresentchange', onVRDisplayPresentChange, false );
+
+	}
+
+	var matrixWorldInverse = new Matrix4();
+	var tempQuaternion = new Quaternion();
+	var tempPosition = new Vector3();
+
+	var cameraL = new PerspectiveCamera();
+	cameraL.viewport = new Vector4();
+	cameraL.layers.enable( 1 );
+
+	var cameraR = new PerspectiveCamera();
+	cameraR.viewport = new Vector4();
+	cameraR.layers.enable( 2 );
+
+	var cameraVR = new ArrayCamera( [ cameraL, cameraR ] );
+	cameraVR.layers.enable( 1 );
+	cameraVR.layers.enable( 2 );
+
+	var currentSize = new Vector2(), currentPixelRatio;
+
+	function onVRDisplayPresentChange() {
+
+		var isPresenting = scope.isPresenting = device !== null && device.isPresenting === true;
+
+		if ( isPresenting ) {
+
+			var eyeParameters = device.getEyeParameters( 'left' );
+			renderWidth = 2 * eyeParameters.renderWidth * framebufferScaleFactor;
+			renderHeight = eyeParameters.renderHeight * framebufferScaleFactor;
+
+			currentPixelRatio = renderer.getPixelRatio();
+			renderer.getSize( currentSize );
+
+			renderer.setDrawingBufferSize( renderWidth, renderHeight, 1 );
+
+			cameraL.viewport.set( 0, 0, renderWidth / 2, renderHeight );
+			cameraR.viewport.set( renderWidth / 2, 0, renderWidth / 2, renderHeight );
+
+			animation.start();
+
+			scope.dispatchEvent( { type: 'sessionstart' } );
+
+		} else {
+
+			if ( scope.enabled ) {
+
+				renderer.setDrawingBufferSize( currentSize.width, currentSize.height, currentPixelRatio );
+
+			}
+
+			animation.stop();
+
+			scope.dispatchEvent( { type: 'sessionend' } );
+
+		}
+
+	}
+
+	//
+
+	var triggers = [];
+	var grips = [];
+
+	function findGamepad( id ) {
+
+		var gamepads = navigator.getGamepads && navigator.getGamepads();
+
+		for ( var i = 0, l = gamepads.length; i < l; i ++ ) {
+
+			var gamepad = gamepads[ i ];
+
+			if ( gamepad && ( gamepad.id === 'Daydream Controller' ||
+				gamepad.id === 'Gear VR Controller' || gamepad.id === 'Oculus Go Controller' ||
+				gamepad.id === 'OpenVR Gamepad' || gamepad.id.startsWith( 'Oculus Touch' ) ||
+				gamepad.id.startsWith( 'HTC Vive Focus' ) ||
+				gamepad.id.startsWith( 'Spatial Controller' ) ) ) {
+
+				var hand = gamepad.hand;
+
+				if ( id === 0 && ( hand === '' || hand === 'right' ) ) return gamepad;
+				if ( id === 1 && ( hand === 'left' ) ) return gamepad;
+
+			}
+
+		}
+
+	}
+
+	function updateControllers() {
+
+		for ( var i = 0; i < controllers.length; i ++ ) {
+
+			var controller = controllers[ i ];
+
+			var gamepad = findGamepad( i );
+
+			if ( gamepad !== undefined && gamepad.pose !== undefined ) {
+
+				if ( gamepad.pose === null ) return;
+
+				// Pose
+
+				var pose = gamepad.pose;
+
+				if ( pose.hasPosition === false ) controller.position.set( 0.2, - 0.6, - 0.05 );
+
+				if ( pose.position !== null ) controller.position.fromArray( pose.position );
+				if ( pose.orientation !== null ) controller.quaternion.fromArray( pose.orientation );
+				controller.matrix.compose( controller.position, controller.quaternion, controller.scale );
+				controller.matrix.premultiply( standingMatrix );
+				controller.matrix.decompose( controller.position, controller.quaternion, controller.scale );
+				controller.matrixWorldNeedsUpdate = true;
+				controller.visible = true;
+
+				// Trigger
+
+				var buttonId = gamepad.id === 'Daydream Controller' ? 0 : 1;
+
+				if ( triggers[ i ] === undefined ) triggers[ i ] = false;
+
+				if ( triggers[ i ] !== gamepad.buttons[ buttonId ].pressed ) {
+
+					triggers[ i ] = gamepad.buttons[ buttonId ].pressed;
+
+					if ( triggers[ i ] === true ) {
+
+						controller.dispatchEvent( { type: 'selectstart' } );
+
+					} else {
+
+						controller.dispatchEvent( { type: 'selectend' } );
+						controller.dispatchEvent( { type: 'select' } );
+
+					}
+
+				}
+
+				// Grip
+				buttonId = 2;
+
+				if ( grips[ i ] === undefined ) grips[ i ] = false;
+
+				// Skip if the grip button doesn't exist on this controller
+				if ( gamepad.buttons[ buttonId ] !== undefined ) {
+
+					if ( grips[ i ] !== gamepad.buttons[ buttonId ].pressed ) {
+
+						grips[ i ] = gamepad.buttons[ buttonId ].pressed;
+
+						if ( grips[ i ] === true ) {
+
+							controller.dispatchEvent( { type: 'squeezestart' } );
+
+						} else {
+
+							controller.dispatchEvent( { type: 'squeezeend' } );
+							controller.dispatchEvent( { type: 'squeeze' } );
+
+						}
+
+					}
+
+				}
+
+			} else {
+
+				controller.visible = false;
+
+			}
+
+		}
+
+	}
+
+	function updateViewportFromBounds( viewport, bounds ) {
+
+		if ( bounds !== null && bounds.length === 4 ) {
+
+			viewport.set( bounds[ 0 ] * renderWidth, bounds[ 1 ] * renderHeight, bounds[ 2 ] * renderWidth, bounds[ 3 ] * renderHeight );
+
+		}
+
+	}
+
+	//
+
+	this.enabled = false;
+
+	this.getController = function ( id ) {
+
+		var controller = controllers[ id ];
+
+		if ( controller === undefined ) {
+
+			controller = new Group();
+			controller.matrixAutoUpdate = false;
+			controller.visible = false;
+
+			controllers[ id ] = controller;
+
+		}
+
+		return controller;
+
+	};
+
+	this.getDevice = function () {
+
+		return device;
+
+	};
+
+	this.setDevice = function ( value ) {
+
+		if ( value !== undefined ) device = value;
+
+		animation.setContext( value );
+
+	};
+
+	this.setFramebufferScaleFactor = function ( value ) {
+
+		framebufferScaleFactor = value;
+
+	};
+
+	this.setReferenceSpaceType = function ( value ) {
+
+		referenceSpaceType = value;
+
+	};
+
+	this.setPoseTarget = function ( object ) {
+
+		if ( object !== undefined ) poseTarget = object;
+
+	};
+
+	this.getCamera = function ( camera ) {
+
+		var userHeight = referenceSpaceType === 'local-floor' ? 1.6 : 0;
+
+		device.depthNear = camera.near;
+		device.depthFar = camera.far;
+
+		device.getFrameData( frameData );
+
+		//
+
+		if ( referenceSpaceType === 'local-floor' ) {
+
+			var stageParameters = device.stageParameters;
+
+			if ( stageParameters ) {
+
+				standingMatrix.fromArray( stageParameters.sittingToStandingTransform );
+
+			} else {
+
+				standingMatrix.makeTranslation( 0, userHeight, 0 );
+
+			}
+
+		}
+
+
+		var pose = frameData.pose;
+		var poseObject = poseTarget !== null ? poseTarget : camera;
+
+		// We want to manipulate poseObject by its position and quaternion components since users may rely on them.
+		poseObject.matrix.copy( standingMatrix );
+		poseObject.matrix.decompose( poseObject.position, poseObject.quaternion, poseObject.scale );
+
+		if ( pose.orientation !== null ) {
+
+			tempQuaternion.fromArray( pose.orientation );
+			poseObject.quaternion.multiply( tempQuaternion );
+
+		}
+
+		if ( pose.position !== null ) {
+
+			tempQuaternion.setFromRotationMatrix( standingMatrix );
+			tempPosition.fromArray( pose.position );
+			tempPosition.applyQuaternion( tempQuaternion );
+			poseObject.position.add( tempPosition );
+
+		}
+
+		poseObject.updateMatrixWorld();
+
+		var children = poseObject.children;
+		for ( var i = 0, l = children.length; i < l; i ++ ) {
+
+			children[ i ].updateMatrixWorld( true );
+
+		}
+
+		//
+
+		cameraL.near = camera.near;
+		cameraR.near = camera.near;
+
+		cameraL.far = camera.far;
+		cameraR.far = camera.far;
+
+		cameraL.matrixWorldInverse.fromArray( frameData.leftViewMatrix );
+		cameraR.matrixWorldInverse.fromArray( frameData.rightViewMatrix );
+
+		// TODO (mrdoob) Double check this code
+
+		standingMatrixInverse.getInverse( standingMatrix );
+
+		if ( referenceSpaceType === 'local-floor' ) {
+
+			cameraL.matrixWorldInverse.multiply( standingMatrixInverse );
+			cameraR.matrixWorldInverse.multiply( standingMatrixInverse );
+
+		}
+
+		var parent = poseObject.parent;
+
+		if ( parent !== null ) {
+
+			matrixWorldInverse.getInverse( parent.matrixWorld );
+
+			cameraL.matrixWorldInverse.multiply( matrixWorldInverse );
+			cameraR.matrixWorldInverse.multiply( matrixWorldInverse );
+
+		}
+
+		// envMap and Mirror needs camera.matrixWorld
+
+		cameraL.matrixWorld.getInverse( cameraL.matrixWorldInverse );
+		cameraR.matrixWorld.getInverse( cameraR.matrixWorldInverse );
+
+		cameraL.projectionMatrix.fromArray( frameData.leftProjectionMatrix );
+		cameraR.projectionMatrix.fromArray( frameData.rightProjectionMatrix );
+
+		setProjectionFromUnion( cameraVR, cameraL, cameraR );
+
+		//
+
+		var layers = device.getLayers();
+
+		if ( layers.length ) {
+
+			var layer = layers[ 0 ];
+
+			updateViewportFromBounds( cameraL.viewport, layer.leftBounds );
+			updateViewportFromBounds( cameraR.viewport, layer.rightBounds );
+
+		}
+
+		updateControllers();
+
+		return cameraVR;
+
+	};
+
+	this.getStandingMatrix = function () {
+
+		return standingMatrix;
+
+	};
+
+	this.isPresenting = false;
+
+	// Animation Loop
+
+	var animation = new WebGLAnimation();
+
+	this.setAnimationLoop = function ( callback ) {
+
+		animation.setAnimationLoop( callback );
+
+		if ( this.isPresenting ) animation.start();
+
+	};
+
+	this.submitFrame = function () {
+
+		if ( this.isPresenting ) device.submitFrame();
+
+	};
+
+	this.dispose = function () {
+
+		if ( typeof window !== 'undefined' ) {
+
+			window.removeEventListener( 'vrdisplaypresentchange', onVRDisplayPresentChange );
+
+		}
+
+	};
+
+	// DEPRECATED
+
+	this.setFrameOfReferenceType = function () {
+
+		console.warn( 'THREE.WebVRManager: setFrameOfReferenceType() has been deprecated.' );
+
+	};
+
+}
+
+Object.assign( WebVRManager.prototype, EventDispatcher.prototype );
 
 /**
  * @author Mugen87 / https://github.com/Mugen87
@@ -23056,6 +23554,7 @@ function WebXRManager( renderer, gl ) {
 	var referenceSpaceType = 'local-floor';
 
 	var pose = null;
+	var poseTarget = null;
 
 	var controllers = [];
 	var inputSourcesMap = new Map();
@@ -23085,7 +23584,13 @@ function WebXRManager( renderer, gl ) {
 
 	this.isPresenting = false;
 
-	this.getController = function ( index ) {
+	this.getCameraPose = function ( ) {
+
+		return pose;
+
+	};
+
+	this.getController = function ( id ) {
 
 		var controller = controllers[ index ];
 
@@ -23362,6 +23867,12 @@ function WebXRManager( renderer, gl ) {
 
 	}
 
+	this.setPoseTarget = function ( object ) {
+
+		if ( object !== undefined ) poseTarget = object;
+
+	};
+
 	this.getCamera = function ( camera ) {
 
 		cameraVR.near = cameraR.near = cameraL.near = camera.near;
@@ -23383,6 +23894,7 @@ function WebXRManager( renderer, gl ) {
 
 		var parent = camera.parent;
 		var cameras = cameraVR.cameras;
+		var object = poseTarget || camera;
 
 		updateCamera( cameraVR, parent );
 
@@ -23394,9 +23906,9 @@ function WebXRManager( renderer, gl ) {
 
 		// update camera and its children
 
-		camera.matrixWorld.copy( cameraVR.matrixWorld );
+		object.matrixWorld.copy( cameraVR.matrixWorld );
 
-		var children = camera.children;
+		var children = object.children;
 
 		for ( var i = 0, l = children.length; i < l; i ++ ) {
 
@@ -23771,8 +24283,7 @@ function WebGLRenderer( parameters ) {
 	initGLContext();
 
 	// xr
-
-	var xr = new WebXRManager( _this, _gl );
+	var xr = ( typeof navigator !== 'undefined' && 'xr' in navigator ) ? new WebXRManager( _this, _gl ) : new WebVRManager( _this );
 
 	this.xr = xr;
 
@@ -24731,6 +25242,12 @@ function WebGLRenderer( parameters ) {
 		state.buffers.color.setMask( true );
 
 		state.setPolygonOffset( false );
+
+		if ( xr.enabled && xr.submitFrame ) {
+
+			xr.submitFrame();
+
+		}
 
 		// _gl.finish();
 
@@ -26046,6 +26563,33 @@ function WebGLRenderer( parameters ) {
 			( material.isShaderMaterial && material.lights === true );
 
 	}
+
+	// this.setTexture2D = setTexture2D;
+	this.setTexture2D = ( function () {
+
+		var warned = false;
+
+		// backwards compatibility: peel texture.texture
+		return function setTexture2D( texture, slot ) {
+
+			if ( texture && texture.isWebGLRenderTarget ) {
+
+				if ( ! warned ) {
+
+					console.warn( "THREE.WebGLRenderer.setTexture2D: don't use render targets as textures. Use their .texture property instead." );
+					warned = true;
+
+				}
+
+				texture = texture.texture;
+
+			}
+
+			textures.setTexture2D( texture, slot );
+
+		};
+
+	}() );
 
 	//
 	this.setFramebuffer = function ( value ) {
@@ -32751,6 +33295,8 @@ function CircleBufferGeometry( radius, segments, thetaStart, thetaLength ) {
 CircleBufferGeometry.prototype = Object.create( BufferGeometry.prototype );
 CircleBufferGeometry.prototype.constructor = CircleBufferGeometry;
 
+
+
 var Geometries = /*#__PURE__*/Object.freeze({
 	__proto__: null,
 	WireframeGeometry: WireframeGeometry,
@@ -33794,6 +34340,8 @@ LineDashedMaterial.prototype.copy = function ( source ) {
 	return this;
 
 };
+
+
 
 var Materials = /*#__PURE__*/Object.freeze({
 	__proto__: null,
@@ -38240,6 +38788,8 @@ SplineCurve.prototype.fromJSON = function ( json ) {
 	return this;
 
 };
+
+
 
 var Curves = /*#__PURE__*/Object.freeze({
 	__proto__: null,
@@ -50235,11 +50785,6 @@ Object.assign( WebGLRenderer.prototype, {
 	setTexture: function () {
 
 		console.warn( 'THREE.WebGLRenderer: .setTexture() has been removed.' );
-
-	},
-	setTexture2D: function () {
-
-		console.warn( 'THREE.WebGLRenderer: .setTexture2D() has been removed.' );
 
 	},
 	setTextureCube: function () {
