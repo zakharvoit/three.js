52a686a3a7621572232220e3411911eed15a0bf3
Diego Marcos <diego.marcos@gmail.com>
2020-11-25 16:50:11 -0800

    Reintroduce LegacyJSONLoader

--- src/loaders/ObjectLoader.js
+++ src/loaders/ObjectLoader.js
@@ -280,7 +280,17 @@ class ObjectLoader extends Loader {
 
 					case 'Geometry':
 
-						console.error( 'THREE.ObjectLoader: The legacy Geometry type is no longer supported.' );
+						if ( 'THREE' in window && 'LegacyJSONLoader' in THREE ) {
+
+						var geometryLoader = new THREE.LegacyJSONLoader();
+						geometry = geometryLoader.parse( data, this.resourcePath ).geometry;
+
+
+						} else {
+
+						       console.error( 'THREE.ObjectLoader: You have to import LegacyJSONLoader in order load geometry data of type "Geometry".' );
+
+						}
 
 						break;
 
